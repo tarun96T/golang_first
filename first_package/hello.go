a95ea3f27f8f809922ef7a1f7e6ef549e41e566b
Tarun <tarsuno@gmail.com>
2021-08-20 08:12:48 +0100

    package_try

--- first_package/hello.go
+++ first_package/hello.go
@@ -3,6 +3,8 @@ package first_package
 
 import "fmt"
 
+Morning string = "good morning"
+
 func Say_hello() {
 
 	fmt.Println("Hello there, general Kenobi")
--- test.go
+++ test.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"/first_package"
+)
 
 func main() {
-	fmt.Println("test was good")
+	first_package.Say_hello()
+	//fmt.Println("test was good")
 }
